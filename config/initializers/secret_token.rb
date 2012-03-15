44848b76577c2d19943dc9f443e768d7129af24c
Gal Dubitski <galmail@gmail.com>
2012-03-15 09:39:22 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TocartaAdmin::Application.config.secret_token = 'faf2a73b180c44969448df1d6dab6625a8c80c298debfc5f107b9718056a6dc9aa615d0c32e5587c06cf487fc9d82dd4c68f3abc660ea09c1a3789a092ddb15f'
