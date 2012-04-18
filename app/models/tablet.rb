36e6a6d99de170be160e5b9de8103c2b51a2f9b3
Gal Dubitski <galmail@gmail.com>
2012-04-18 11:07:32 +0200

    random access key

--- app/models/tablet.rb
+++ app/models/tablet.rb
@@ -2,10 +2,15 @@ class Tablet < ActiveRecord::Base
   belongs_to :table
 	has_many :orders
 	attr_accessible :name, :active, :activated, :access_key, :display_size, :device_brand, :device_name, :device_os, :last_menu_sync, :alive, :table_id
+	after_initialize :init
 	
 	### Validations ###
 	
 	validates :access_key, :table_id, :presence => true
 	validates :access_key, :uniqueness => true
+	
+	def init
+	  self.access_key ||= (0...8).map{65.+(rand(25)).chr}.join
+	end
 	
 end
