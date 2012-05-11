2f250d9eff7fda63c948e1a0a419ad76986baa00
Gal Dubitski <galmail@gmail.com>
2012-05-11 10:28:49 +0200

    db relations changed for dishes

--- db/migrate/20120424235211_remove_columns_section_and_subsection_from_dishes.rb
+++ db/migrate/20120424235211_remove_columns_section_and_subsection_from_dishes.rb
@@ -1,6 +1,14 @@
 class RemoveColumnsSectionAndSubsectionFromDishes < ActiveRecord::Migration
-  def change
+  def up
     remove_column :dishes, :section_id
     remove_column :dishes, :subsection_id
+  end
+  def down
+    change_table :dishes do |t|
+      t.references :section
+      t.references :subsection
+    end
+    add_index :dishes, :section_id
+    add_index :dishes, :subsection_id
   end
 end
