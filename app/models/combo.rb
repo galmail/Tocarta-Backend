79ec1bbb6c2a48c4860eb2d3c05a9b8236d3ef89
Gal Dubitski <galmail@gmail.com>
2012-05-14 01:49:26 +0200

    migration

--- app/models/combo.rb
+++ app/models/combo.rb
@@ -1,7 +1,7 @@
 class Combo < ActiveRecord::Base
   # FIXME uncomment these lines after migration
-  # has_many :dishes, :through => :dish_combo_associations
-  # has_many :dish_combo_associations
+  has_many :dishes, :through => :dish_combo_associations
+  has_many :dish_combo_associations
   
   belongs_to :combo_type
   belongs_to :restaurant
