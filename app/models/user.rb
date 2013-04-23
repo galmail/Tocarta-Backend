db431c94c0a303809fb04e81d9d73fb8f303c559
Hegel Trigo <hegel.trigo@syscrunch.com>
2013-04-23 11:55:20 -0400

    add dishes for create demo restaurant

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :role
 
-  after_create :make_restaurant
+  after_create :make_restaurant_demo
 
   require 'open-uri'
 
@@ -21,14 +21,21 @@ class User < ActiveRecord::Base
 	has_one :restaurant
 	has_one :client
 
-  def make_restaurant
-    if self.role == 'restaurant'
-      #create chain, restaurant and two dishes for demo
-      chain = Chain.create(user_id: self.id, name: "Mi Cadena de Restaurantes #{self.email}", logo: File.open("#{Rails.root}/app/assets/images/restaurant-logo.jpg"), email: self.email)
-      Restaurant.create(user_id: self.id, chain_id: chain.id, name: 'Mi Restaurante Demo', email: self.email)
-      Dish.create(name: 'principal', photo: File.open("#{Rails.root}/app/assets/images/dish-1.jpg"), active: true, rate_me: true, description: 'plato principal', price: 20, chain_id: chain.id, position: 1)
-      Dish.create(name: 'segundo', photo: File.open("#{Rails.root}/app/assets/images/dish-2.jpg"), active: true, rate_me: true, description: 'plato principal', price: 15, chain_id: chain.id, position: 1)
+  def make_restaurant_demo
+    if self.role == 'restaurant' # only for this role
+       #create a restaurant and dependencies
+       chain = Chain.create(user_id: self.id, name: "Mi Cadena de Restaurantes #{self.email}", logo: File.open("#{Rails.root}/app/assets/images/restaurant-logo.jpg"), email: self.email)
+       restaurant = Restaurant.create(user_id: self.id, chain_id: chain.id, name: 'Mi Restaurante Demo', email: self.email)
+       menu = Menu.create(restaurant_id: restaurant.id, name: "Mi Menu", menu_type: 'main')
+       section = Section.create(menu_id: menu.id, name: 'Mi Seccion', active: true, photo: File.open("#{Rails.root}/app/assets/images/section.jpg"))
+       make_dishes_demo(chain, section)
     end
+  end
+
+  def make_dishes_demo(chain, section)
+    # Two dishes for the restaurant
+    Dish.create(name: 'principal', photo: File.open("#{Rails.root}/app/assets/images/dish-1.jpg"), active: true, rate_me: true, description: 'plato principal', price: 20, chain_id: chain.id, position: 1, section_ids: [section.id])
+    Dish.create(name: 'segundo', photo: File.open("#{Rails.root}/app/assets/images/dish-2.jpg"), active: true, rate_me: true, description: 'plato principal', price: 15, chain_id: chain.id, position: 1, section_ids: [section.id])
   end
   
 end
