ed6bb2d222a1f7920f4313c6674992aa46ac8074
Hegel Trigo <hegel.trigo@syscrunch.com>
2013-04-24 12:55:10 -0400

    change to import image from url

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,19 +23,19 @@ class User < ActiveRecord::Base
 
   def make_restaurant_demo
     if self.role == 'restaurant' # only for this role
-       #create a restaurant and dependencies
-       chain = Chain.create(user_id: self.id, name: "Mi Cadena de Restaurantes #{self.email}", logo: File.open("#{Rails.root}/app/assets/images/restaurant-logo.jpg"), email: self.email)
+       #create a restaurant and dependencies (chain, section, menu)
+       chain = Chain.create(user_id: self.id, name: "Mi Cadena de Restaurantes #{self.email}", logo: URI.parse("http://openclipart.org/image/800px/svg_to_png/18397/geant_pictogram_restaurant.png"), email: self.email)
        restaurant = Restaurant.create(user_id: self.id, chain_id: chain.id, name: 'Mi Restaurante Demo', email: self.email)
        menu = Menu.create(restaurant_id: restaurant.id, name: "Mi Menu", menu_type: 'main')
-       section = Section.create(menu_id: menu.id, name: 'Mi Seccion', active: true, photo: File.open("#{Rails.root}/app/assets/images/section.jpg"))
+       section = Section.create(menu_id: menu.id, name: 'Mi Seccion', active: true, photo: URI.parse("http://openclipart.org/image/800px/svg_to_png/8181/johnny_automatic_wine_and_bread.png"))
        make_dishes_demo(chain, section)
     end
   end
 
   def make_dishes_demo(chain, section)
     # Two dishes for the restaurant
-    Dish.create(name: 'principal', photo: File.open("#{Rails.root}/app/assets/images/dish-1.jpg"), active: true, rate_me: true, description: 'plato principal', price: 20, chain_id: chain.id, position: 1, section_ids: [section.id])
-    Dish.create(name: 'segundo', photo: File.open("#{Rails.root}/app/assets/images/dish-2.jpg"), active: true, rate_me: true, description: 'plato principal', price: 15, chain_id: chain.id, position: 1, section_ids: [section.id])
+    Dish.create(name: 'principal', photo: URI.parse("http://openclipart.org/image/800px/svg_to_png/8202/johnny_automatic_oatmeal.png"), active: true, rate_me: true, description: 'plato principal', price: 20, chain_id: chain.id, position: 1, section_ids: [section.id])
+    Dish.create(name: 'segundo', photo: URI.parse("http://openclipart.org/image/800px/svg_to_png/8202/johnny_automatic_oatmeal.png"), active: true, rate_me: true, description: 'plato principal', price: 15, chain_id: chain.id, position: 1, section_ids: [section.id])
   end
   
 end
