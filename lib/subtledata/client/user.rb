ebf3d6060a463bc6e7c8d72c4896a0c8768535e0
Jose Alberto <ja@josealberto.org>
2013-05-17 19:51:21 +0200

    improved docs

--- lib/subtledata/client/user.rb
+++ lib/subtledata/client/user.rb
@@ -5,12 +5,14 @@ module Subtledata
       # Create a user
       # @param args [Array] See subtledata api for arguments
       # @return [Array] See Subtledata api
+      # @example sd.create_user %w(name04 1234 fn mn ln 01/01/2000 111111111 e@mail.com 0 0 1234567890)
       def create_user(*args)
         basic_check(args)
         r = get(@base, { "0110#{@session_token}" => args } )
       end
 
       # Authenticate user
+      # @example sd.auth_user %w(name04 1234 0 0 1234567890)
       def auth_user(*args)
         basic_check(args)
         r = get(@base, { "0111#{@session_token}" => args } )
