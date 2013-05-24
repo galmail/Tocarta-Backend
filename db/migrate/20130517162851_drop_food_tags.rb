5ba7845d2c6ce55035e9212d1905b5712e634c82
Gal Dubitski <galmail@gmail.com>
2013-05-24 17:36:43 +0200

    fixed migration

--- db/migrate/20130517162851_drop_food_tags.rb
+++ db/migrate/20130517162851_drop_food_tags.rb
@@ -1,6 +1,10 @@
 class DropFoodTags < ActiveRecord::Migration
   def up
-    drop_table :food_tags
-    # FoodTag.drop_translation_table!
+    begin
+      drop_table :food_tags if self.table_exists?("combo_types")
+      FoodTag.drop_translation_table!
+    rescue
+      
+    end
   end
 end
