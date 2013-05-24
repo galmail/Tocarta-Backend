ae8e9dbef0d3c436b4dead647e598a128327ac6c
Gal Dubitski <galmail@gmail.com>
2013-05-24 13:44:45 +0200

    rest banners translated

--- app/models/restaurant_banner.rb
+++ app/models/restaurant_banner.rb
@@ -20,9 +20,11 @@ class RestaurantBanner < ActiveRecord::Base
   
   has_attached_file(
    :photo,
-   :path => ":chain_rest_id/img/banners/:style/banner_:id.:extension",
+   :path => ":locale/:chain_rest_id/img/banners/:style/banner_:id.:extension",
    :styles => { :banner => TocartaAdmin::Application::IMAGE_BANNER_SIZE }
   )
+  
+  translates :photo_file_name, :photo_content_type
   
   attr_accessible :name, :position, :active, :photo
   attr_accessible :restaurant_id
