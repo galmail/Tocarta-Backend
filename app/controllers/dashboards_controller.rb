bc79ebb62f882d2eeb0ff3492e551b966d1cb538
Jose Alberto <ja@josealberto.org>
2013-06-10 13:21:40 +0200

    add moderation to dishes comments via ajax

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -29,6 +29,7 @@ class DashboardsController < ApplicationController
       }
     }
   end
+
   def chart_top_dishes
     if params[:reverse]
       dishes = Dish.to_bottom_rating_hash_by_chain(params[:chain])
@@ -36,18 +37,6 @@ class DashboardsController < ApplicationController
       dishes = Dish.to_top_rating_hash_by_chain(params[:chain])
     end
     render :json => dishes
-    # render :json => {
-      # :type => 'ColumnChart',
-      # :cols => [['string', 'Dish'], ['number', 'Average'], ['number', 'Comments']],
-      # :rows => dishes,
-      # :options => {
-        # chartArea: { width: '80%', height: '75%' },
-        # seriesType: 'bars',
-        # series: {1 => {type: 'line'}},
-        # :hAxis => { title: 'dish' },
-        # legend: 'bottom',
-      # }
-    # }
   end
 
   def chart_comments_by
@@ -58,6 +47,26 @@ class DashboardsController < ApplicationController
     comments = Restaurant.find(params[:restaurant]).comments.group_by_day(:created_at).count if range == 'day'
 
     render :json => comments
+  end
+
+  # FIXME: Here temporary, this must be in comments controller
+  def approve_comment
+    @comment = Comment.find params[:comment_id]
+    @comment.approved = true
+    if @comment.save
+      respond_to do |format|
+        format.js {render 'approve_comment'}
+      end
+    end
+  end
+  def reject_comment
+    @comment = Comment.find params[:comment_id]
+    @comment.approved = false
+    if @comment.save
+      respond_to do |format|
+        format.js {render 'reject_comment'}
+      end
+    end
   end
 
   private
