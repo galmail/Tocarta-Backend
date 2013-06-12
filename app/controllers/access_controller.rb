4d7c75112cbd87bfea8f5934d8de8d95bf054c7b
Gal Dubitski <galmail@gmail.com>
2013-06-12 12:35:58 +0200

    added options header

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -17,7 +17,7 @@ class AccessController < ApplicationController
   
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Max-Age'] = "1728000"
   end
   
