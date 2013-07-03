1be8a7cdc94f082008d7baf46aeb735ac533d2d2
Jose Alberto <ja@josealberto.org>
2013-07-03 12:03:37 +0200

    fixed cancan access to RA dashboard

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
 
     can :access, :rails_admin   # grant access to rails_admin
-    # can :dashboard              # grant access to the dashboard
+    can :dashboard              # grant access to the dashboard
 
     #cannot [:update, :destroy], User, :email => 'username@example.com' #testing
 
